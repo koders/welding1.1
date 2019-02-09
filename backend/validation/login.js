840eddc5b792af1f22dd7da5926029477e1c9536
Rihards Fridrihsons <rfridrihsons@gmail.com>
2019-02-10 00:40:59 +0200

    Add backend for login

---
+++ backend/validation/login.js
@@ -0,0 +1,25 @@
+const Validator = require('validator');
+const isEmpty = require('./isEmpty');
+
+module.exports = function validateLoginInput(data) {
+    let errors = {};
+    data.username = !isEmpty(data.username) ? data.username : '';
+    data.password = !isEmpty(data.password) ? data.password : '';
+
+    if(Validator.isEmpty(data.username)) {
+        errors.username = 'Username is required';
+    }
+
+    if(!Validator.isLength(data.password, {min: 6, max: 30})) {
+        errors.password = 'Password must have 6 chars';
+    }
+
+    if(Validator.isEmpty(data.password)) {
+        errors.password = 'Password is required';
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}

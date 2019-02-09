2b2e188c95ccec655478327e9d32406a49bf8499
Rihards Fridrihsons <rfridrihsons@gmail.com>
2019-02-09 23:40:03 +0200

    Add react router and sidebar bar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+import './Themify/themify-icons.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div>
+                    <div className="sidebar">
+                        <div className="logo">
+                            <img src="/gjerde-logo.png" alt="logo" />
+                        </div>
+                        <div className="menu">
+                            <div className="item"><Link to="/">Dashboard</Link></div>
+                            <div className="item"><Link to="/orders">Orders</Link></div>
+                        </div>
+                    </div>
+
+                    <div className="page-container">
+                        <Route exact path="/" component={Home} />
+                        <Route path="/orders" component={Orders} />
+                    </div>
+                </div>
+            </Router>
+        );
+    }
 }
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const Orders = () => (
+    <div>
+        <h2>Orders</h2>
+    </div>
+);
 
 export default App;
